4f5308e6028e4785b167253cc2a7a97f41aa8191
kelechifletcher <kelechi@gofletcher.com>
2016-06-05 16:01:27 -0500

    - batch file support

--- pcommons.py
+++ pcommons.py
@@ -1,14 +1,21 @@
-#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+#!/usr/bin/python
 
+from __future__ import division
 import sys
 import requests
+from optparse import OptionParser
 
 
 class Graph:
+    name = ''
+    description = ''
     gene_list = []
     edge_list = []
 
-    def __init__(self, g_list=None, e_list=None):
+    def __init__(self, name='', description='', g_list=None, e_list=None):
+        self.name = name
+        self.description = description
+
         if g_list:
             self.gene_list = g_list
         else:
@@ -18,6 +25,31 @@ class Graph:
             self.edge_list = e_list
         else:
             self.edge_list = []
+
+    def to_string(self):
+        string = ''
+
+        #essentials
+        score_type = '! Binary'
+        species = '@ Homo Sapiens'
+        gene_id_type = '% Gene Symbol'
+        abbr = ': '
+        name = '= '
+        desc = '+ '
+
+        string += '%s\n%s\n%s\n%s\n%s\n%s\n\n' % (score_type, species,
+                                                gene_id_type, abbr + self.name,
+                                                name + self.name, desc + self.description)
+
+        for gene in self.gene_list:
+            string += '%s\n' % gene
+
+        string += '\n'
+
+        for edge in self.edge_list:
+            string += '%s\t%s\n' % (edge[0], edge[2])
+
+        return string
 
 
 def get_graph(source):
@@ -53,24 +85,46 @@ def get_graph(source):
             if edge[0] in gene_list and edge[2] in gene_list:
                 edge_list.append(edge[:3])
 
-    return gene_list, edge_list
+    return Graph(name='%s Gene Interaction Network' % source,
+                 description='%s Gene Interaction Network Retrieved From Pathway Commons',
+                 g_list=gene_list,
+                 e_list=edge_list)
 
 
 def main(argv):
-    if len(argv) > 0:
-        for source in argv:
-            (gene_list, edge_list) = get_graph(source)
+    usage = 'usage: %s [options] <gene id> or <unigene id>' % argv[0]
+    parser = OptionParser(usage=usage)
 
-            if gene_list and edge_list:
-                v = len(gene_list)
-                e = len(edge_list)
-                density = (2 * e) / (v * (v - 1))
+    parser.add_option('-i', '--info', dest='info', default=False, action='store_true',
+                      help='report # of vertices and edges, graph density')
 
-                print('V: %i\nE: %i\ndensity: %f' % (v, e, density))
+    parser.add_option('-f', '--file', dest='file', action='store',
+                      help='write graph(s) to file')
 
-            else:
-                print('\'%s\' was not found.' % source)
+    (opts, args) = parser.parse_args()
+
+    if len(args) > 0:
+        for source in args:
+            graph = get_graph(source)
+
+            if opts.info:
+                if graph.gene_list:
+                    v = len(graph.gene_list)
+                    e = len(graph.edge_list)
+                    density = (2 * e) / (v * (v - 1))
+
+                    print('V: %i\nE: %i\ndensity: %f' % (v, e, density))
+
+                else:
+                    print('\'%s\' was not found.' % source)
+
+            if opts.file:
+                out_file = open(opts.file, 'w+')
+                out_file.write(graph.to_string())
+                out_file.close()
+
+                print(out_file.name)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main(sys.argv)
